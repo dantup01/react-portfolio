a429c955bf616a77adfcd37600d72e39b5eaa1fa
Daniel Tuppen <daniel.tuppen1@gmail.com>
2021-03-19 18:58:32 +0000

    updated colours

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -62,7 +62,7 @@ const Projects = () => {
                         <a
                           target="_blank"
                           rel="noopener noreferrer"
-                          className="cta-btn text-color-main"
+                          className="cta-btn light-blue-text"
                           href={repo}
                         >
                           Source Code
